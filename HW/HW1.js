66561798e9d103aa620213c36a8986cecfec84a9
Black <max4yp@gmail.com>
2024-05-03 18:07:44 +0300

    1

---
+++ HW/HW1.js
@@ -0,0 +1,49 @@
+let hello ='hello'
+let owu ='owu'
+let com ='com'
+let ua ='ua'
+let n1 =1
+let n10 =10
+let n999 =-999
+let n123 =123
+let p =3.14
+let n2 =2.7
+let n16 =16
+let t =true
+let f =false
+
+console.log(hello)
+console.log(owu)
+console.log(com)
+console.log(ua)
+console.log(n1)
+console.log(n10)
+console.log(n999)
+console.log(n123)
+console.log(p)
+console.log(n2)
+console.log(n16)
+console.log(t)
+console.log(f)
+//////////////////////////////////////////////////////////////////////////
+let firstName = 'Max'
+let middleName ='Chupakhin'
+let lastName = 'Serghiyovich'
+let person = `${firstName} ${middleName} ${lastName}`
+console.log(person)
+/////////////////////////////////////////////////////////////////////////////
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+/////////////////////////////////////////////////////////////////////////////
+let space=' '
+let name=prompt('Enter your name')
+let lastn=prompt('Enter lastname')
+let age=prompt('Enter your age')
+
+console.log(name + space+ lastn + space + age)
